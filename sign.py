32d339f3e51774099ba62b00c1fa4548898cfe1b
Gogs <gogs@fake.local>
2016-11-25 17:14:51 +0800

    init

---
+++ sign.py
@@ -0,0 +1,30 @@
+#cpding=utf-8
+from ghost import Ghost
+import requests
+
+def signYaohuo():
+    ghost = Ghost()
+    session = ghost.start()
+    resp = session.open("http://yaohuo.me/waplogin.aspx")
+    session.set_field_value("[name=logname]", "18940918305")
+    session.set_field_value("[name=logpass]", "82809391996")
+    session.click("[type=submit]",btn=0)
+    session.wait_for_selector("[href='/signin/signin.aspx']")
+    session.open("http://yaohuo.me/signin/signin.aspx")
+    session.set_field_value("[name=content]", "today is a new day")
+    session.click("[name=g]")
+    session.wait_for_selector(".tip")
+    return session.evaluate("document.documentElement.getElementsByTagName('head')")
+def signNeu6():
+    ghost = Ghost()
+    session = ghost.start()
+    resp = session.open("http://bt.neu6.edu.cn/member.php")
+    session.set_field_value("[name=username]", "670932228@qq.com")
+    session.set_field_value("[name=password]", "82809391996")
+    session.click("[type=checkbox]")
+    session.click("[type=submit]")
+    session.open("http://bt.neu6.edu.cn/member.php")
+    return session
+
+#print signYaohuo()
+signNeu6()
